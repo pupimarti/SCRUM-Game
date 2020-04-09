d44b2765225acbecad2ee89ca1eec4b5e452514f
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-04-09 18:24:10 -0300

    order folders

--- src/Body/index.js
+++ src/Body/index.js
@@ -1,29 +1,12 @@
-import React from 'react'
-import './css.css';
-import img from './scrumImage.png'
+import React from 'react';
+import WhatItIs from './WhatItIs';
+import Stores from './Stores';
 
 export default function Body() {
     return (
-        <section className="relleno content-width">
-            <div class = "title">
-                <h2>¿Qué es Scrum Game?</h2>
-            </div> 
-                <div class = "displayFlex">
-                    <div class = "displayColumn" >
-                        <h3>Aprende de forma inteligente</h3>
-                        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
-                            dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
-                            
-                        </p>
-                        <h3>Súper intuitivo y eficaz</h3>
-                        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
-                            dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.. 
-                        </p>
-                    </div>
-                    <div class = "displayColumn">
-                        <img class = "imgScrum" src={img} alt="Scrum"/>
-                    </div>
-                </div>
-        </section>
+        <div>
+            <WhatItIs />
+            <Stores />
+        </div>
     )
 }

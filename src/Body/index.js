842a8ea23ccd689ddc28a1bd426edcb0e2e56e1b
octavio <octi-octi@hotmail.com>
2020-04-11 11:46:35 -0300

    lpm

--- src/Body/index.js
+++ src/Body/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import WhatItIs from './WhatItIs';
 import Stores from './Stores';
-<<<<<<< HEAD
-=======
-import Us from './Us';
->>>>>>> dedd3f93673e0c8d88b8ddc91bedd5dd3a742793
 import News from './News';
+import Us from './Us';
 
 export default function Body() {
     return (
         <div>
             <WhatItIs />
             <Stores />
-<<<<<<< HEAD
-=======
-            <Us />
->>>>>>> dedd3f93673e0c8d88b8ddc91bedd5dd3a742793
             <News />
+            <Us />
         </div>
     )
 }

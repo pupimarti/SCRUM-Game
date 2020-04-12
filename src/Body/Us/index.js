cfbbce93227b9cd9bd9c55f781c45e24b82b1c8b
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-04-12 14:26:51 -0300

    Edit us

--- src/Body/Us/index.js
+++ src/Body/Us/index.js
@@ -14,7 +14,7 @@ export default function Us() {
                 <Circle img={img} name="Juan Agustín Martí" position="Da ordenes" facebook="pupimarti" instagram="pupi_marti" linkedin="pupimarti"/>
                 <Circle img="https://www.webcampus.uade.edu.ar/Fotos/1/ID_993091.jpg" name="Amietta, Octavio" position="Hace el café" mail="pupi_marti@hotmail.com" github="pupimarti" twitter="pupi_marti"/>
                 <Circle img="https://www.webcampus.uade.edu.ar/Fotos/7/ID_990787.jpg" name="Cretari, Nahuel" position="Sirve agua" whatsapp="5492245427854"/>
-                <Circle img="https://www.webcampus.uade.edu.ar/Fotos/3/ID_992283.jpg" name="Daffada, Agustín" position="Aguatero Suplente"/>
+                <Circle img="https://www.webcampus.uade.edu.ar/Fotos/3/ID_992283.jpg" name="Daffada, Agustín" position="Carga el agua"/>
             </div>
         </div>
     )

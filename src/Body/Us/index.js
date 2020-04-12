05e4fadd6a315009c7e60e111f900f045ec1b819
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-04-12 14:26:10 -0300

    Edit us

--- src/Body/Us/index.js
+++ src/Body/Us/index.js
@@ -5,16 +5,16 @@ import img from './1.jpg';
 
 export default function Us() {
     return (
-        <div className="content-width content-section content-us">
+        <div className="content-width content-section content-us"
+        data-aos="fade-left" 
+        data-aos-offset="300"
+        data-aos-duration="1000" >
             <h2>Nuestro equipo</h2>
             <div className="content-members">
-                <Circle img={img} name="Juan Agustín Martí" position="Web frontend" facebook="pupimarti" instagram="pupi_marti" linkedin="pupimarti"/>
-                <Circle img={img} name="Juan Agustín Martí" position="Hace el café" mail="pupi_marti@hotmail.com" github="pupimarti" twitter="pupi_marti"/>
-                <Circle img={img} name="Juan Agustín Martí" position="Sirve agua" whatsapp="5492245427854"/>
-                <Circle img={img} name="Juan Agustín Martí" position="Limpia"/>
-                <Circle img={img} name="Juan Agustín Martí" position="Se manda cagadas"/>
-                <Circle img={img} name="Juan Agustín Martí" position="Arregla las cagadas"/>
-                <Circle img={img} name="Juan Agustín Martí" position="Duerme"/>
+                <Circle img={img} name="Juan Agustín Martí" position="Da ordenes" facebook="pupimarti" instagram="pupi_marti" linkedin="pupimarti"/>
+                <Circle img="https://www.webcampus.uade.edu.ar/Fotos/1/ID_993091.jpg" name="Amietta, Octavio" position="Hace el café" mail="pupi_marti@hotmail.com" github="pupimarti" twitter="pupi_marti"/>
+                <Circle img="https://www.webcampus.uade.edu.ar/Fotos/7/ID_990787.jpg" name="Cretari, Nahuel" position="Sirve agua" whatsapp="5492245427854"/>
+                <Circle img="https://www.webcampus.uade.edu.ar/Fotos/3/ID_992283.jpg" name="Daffada, Agustín" position="Aguatero Suplente"/>
             </div>
         </div>
     )

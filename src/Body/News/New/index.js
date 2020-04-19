5541b16eda18cfe97e6e9397053845141aa90860
octavio <octi-octi@hotmail.com>
2020-04-19 12:39:17 -0300

    added editable News Section

--- src/Body/News/New/index.js
+++ src/Body/News/New/index.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import ReactModal from 'react-modal';
 import './css.css';
 
-export default function Modal({title, text}) {
+export default function Modal({edit, title, text}) {
 
     const [open, setOpen] = useState(false);
    
@@ -10,14 +10,31 @@ export default function Modal({title, text}) {
     const handleCloseModal = () =>setOpen(false)
     const handleOpenModal = () =>setOpen(true)
 
+    const [data, setData] = useState({title, text});
+
+    const handleDefaultProps = () => {
+        setData({title,  text})
+    }
+
+    const handleInputChange = (e) => {
+        const target = e.target;
+        const name = target.name;
+        setData({...data, [name]: target.value})
+    }
+
+    const handleClickCancel = () => {
+        handleDefaultProps();
+    }
    
 
     ReactModal.setAppElement('#root')
 
-   
+   if (edit)
     return (
         <React.Fragment>
+            
             <h3 className="new-title" onClick={handleOpenModal}>{title}</h3>
+            
             <ReactModal
              isOpen={open}
              onRequestClose={handleCloseModal}
@@ -25,11 +42,49 @@ export default function Modal({title, text}) {
              overlayClassName="Overlay"
              >
               <div className="modal-content-new">
-                  <h2 className="modal-title">{title}</h2>
-                  <p className="modal-text">{text}</p>
+                    <input
+                        type="text"
+                        name="title"
+                        onChange={handleInputChange}
+                        value={data.title}
+                        className="new-title"
+                    />
+                    <textarea 
+                        type="text" 
+                        name="text" 
+                        onChange={handleInputChange} 
+                        value={data.text} 
+                        className="modal-text"
+                    />
+              </div>
+      
+              <button className="button modal-close" onClick={handleCloseModal}>GUARDAR SECCIÓN</button>
+              <button className="button modal-close" onClick={handleClickCancel && handleCloseModal}>CANCELAR</button>
+              
+             </ReactModal>
+            
+           
+        </React.Fragment>
+    )
+    else 
+    return (
+    <React.Fragment>
+            
+           <h3 className="new-title" onClick={handleOpenModal}>{title}</h3>
+            <ReactModal
+             isOpen={open}
+             onRequestClose={handleCloseModal}
+             className="Modal"
+             overlayClassName="Overlay"
+             >
+              <div className="modal-content-new">
+                    <h2 className="modal-title">{title}</h2>
+                    <p className="modal-text">{text}</p>
               </div>
               <button className="button modal-close" onClick={handleCloseModal}>CERRAR</button>
              </ReactModal>
+            
+           
         </React.Fragment>
     )
     
--- src/Body/News/index.js
+++ src/Body/News/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import New from './New';
 import './css.css';
 
-export default function News() {
+export default function News(props) {
   return (
     <div className="content-section content-width content-section-news"
       data-aos="fade-left" 
@@ -11,14 +11,38 @@ export default function News() {
       >
             <h2>Novedades</h2>
             <div className="content-news">
-              <New title="El primer modal AA PRUEBA TEXTO MUY LARGO LOREM LOREM" text="Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas"/>
-              <New title="el segundo modal" text="Modal 2 texto"/>
-              <New title="El tercer modal" text="Hola"/>
-              <New title="el cuarto modal" text="Cuarto modal"/>
-              <New title="El QUINTO modal" text="QUINTO"/>
-              <New title="el SEXTO modal" text="SEXTO modal"/>
-              <New title="El SEPTIMO modal" text="SEPTIMO"/>
-              <New title="el OCTAVO modal" text="OCTAVO modal"/>
+              <New 
+                  edit={props.edit} 
+                  title="El primer modal AA PRUEBA TEXTO MUY LARGO LOREM LOREM" 
+                  text="Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas"/>
+              <New 
+                  edit={props.edit}
+                  title="el segundo modal" 
+                  text="Modal 2 texto"/>
+              <New 
+                  edit={props.edit}
+                  title="El tercer modal" 
+                  text="Hola"/>
+              <New 
+                  edit={props.edit}
+                  title="el cuarto modal" 
+                  text="Cuarto modal"/>
+              <New 
+                  edit={props.edit}
+                  title="El QUINTO modal" 
+                  text="QUINTO"/>
+              <New 
+                  edit={props.edit}
+                  title="el SEXTO modal" 
+                  text="SEXTO modal"/>
+              <New 
+                  edit={props.edit}
+                  title="El SEPTIMO modal" 
+                  text="SEPTIMO"/>
+              <New 
+                  edit={props.edit}
+                  title="el OCTAVO modal" 
+                  text="OCTAVO modal"/>
             </div>
     </div>
   )
